061f3347c9c211335dfd025c93dbce96ac9256c3
Permafacture <Permafacture@gmail.com>
2009-09-11 17:38:53 -0500

    Added rectangular face

--- raytrace/faces.py
+++ raytrace/faces.py
@@ -26,6 +26,7 @@ import numpy
 
 from raytrace.utils import Convert_to_SP, dotprod, transformPoints,\
         transformNormals
+        
 from raytrace.rays import RayCollection
 
 class Face(HasTraits):
@@ -132,7 +133,56 @@ class CircularFace(Face):
         n = numpy.asarray([t.transform_vector(0,0,-1),])
         return numpy.ones(points.shape) * n
     
-
+class RectangularFace(Face):
+    name = "rectangular face"
+    length = PrototypedFrom('owner')
+    width = PrototypedFrom('owner')
+    #offset = PrototypedFrom('owner')
+    
+    def intersect(self, P1, P2, max_lenth):
+        """
+        Calculate intersection point for a ray segment between two points
+        P1 and P2, in the local coordinate system.
+        """
+        max_length = numpy.sqrt(((P2 - P1)**2).sum(axis=1))
+        len = self.length
+        wid = self.width
+        #offset = self.offset
+        
+        x1,y1,z1 = P1.T
+        x2,y2,z2 = P2.T
+        
+        h = -z1/(z2-z1)
+        X = x1 + h*(x2-x1) #- offset
+        Y = y1 + h*(y2-y1)
+        
+        length = max_length*h
+        
+        mask = Y > len
+        mask = numpy.logical_or(mask, Y < 0)
+        mask = numpy.logical_or(mask, X > wid/2)
+        mask = numpy.logical_or(mask, X < -wid/2)
+        mask = numpy.logical_or(mask, h < self.tolerance)
+        mask = numpy.logical_or(mask, h > 1.0)
+        
+        length[mask] = numpy.Infinity
+        
+        # was x + offset before i removed offsets
+        t_points = numpy.column_stack((X, Y, numpy.zeros_like(X)))
+    
+        dtype=([('length','f8'),('face', 'O'),('point','f8',3)])
+        result = numpy.empty(P1.shape[0], dtype=dtype)
+        result['length'] = length
+        result['face'] = self
+        result['point'] = t_points
+        return result
+    
+    def compute_normal(self, points):
+        """computes the surface normal in the global coordinate system"""
+        t = self.transform
+        n = numpy.asarray([t.transform_vector(0,0,-1),])
+        return numpy.ones(points.shape) * n
+        
 class PolygonFace(Face):
     name = "polygon face"
     diameter = PrototypedFrom('owner')
