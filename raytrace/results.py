f9771206ae4763bc67925329d06402769212b593
Permafacture <Permafacture@gmail.com>
2009-09-18 18:33:37 -0500

    Added a collection efficency result that uses an aperture object and a target.

--- raytrace/results.py
+++ raytrace/results.py
@@ -3,12 +3,13 @@ A module for Results subclasses
 """
 
 from enthought.traits.api import Instance, Float, on_trait_change,\
-            Button
+            Button, Int
 
 from enthought.traits.ui.api import View, Item, DropEditor
 
 from raytrace.bases import Result
 from raytrace.faces import Face
+from raytrace.sources import BaseRaySource
 from raytrace.tracer import RayTraceModel
 from enthought.traits.ui.editors.drop_editor import DropEditor
 
@@ -16,6 +17,23 @@ import numpy
 
 def get_total_intersections(raysList, face):
     return sum((rays.end_face==face).sum() for rays in raysList)
+
+def get_total_entries(raysList, face):
+    
+    total = 0
+    for rays in raysList:
+        a = numpy.zeros(rays.end_face.shape)
+        for i,norm in enumerate(rays.normals):
+            a[i] = numpy.vdot(rays.direction[i],norm)>0
+            #print "et: ",norm
+            #print rays.direction[i]
+        b = rays.face==face
+        
+
+        c = sum(numpy.logical_and(a,b)).sum() 
+        total = total + c
+    
+    return total
 
 
 class Ratio(Result):
@@ -62,6 +80,8 @@ class Ratio(Result):
         #instead, just take the first source found
         source = self._tracer.sources[0]
         
+        print source
+        
         #a list of RayCollections
         raysList = source.TracedRays
         
@@ -72,4 +92,52 @@ class Ratio(Result):
             self.result = float(nom_count)/float(denom_count)
         except ZeroDivisionError:
             self.result = numpy.Infinity
+
+class Total_Efficency(Result):
+    name = "total efficency"
+    Target = Instance(Face)
+    Aperture = Instance(Face)
+    
+    result = Float(label="Collection Efficency")
+    
+    _tracer = Instance(RayTraceModel) #to cache the tracer instance
+    
+    traits_view = View(Item('result', style="readonly"),
+                       Item('Aperture', editor=DropEditor()),
+                       Item('Target', editor=DropEditor()),
+                       title="Target/Entry ratio",
+                       resizable=True,
+                       )
+    
+    @on_trait_change("Aperture, Target, _tracer")
+    def update(self):
+        if self._tracer is not None:
+            self._calc_result()
         
+    def calc_result(self, tracer):
+        self._tracer = tracer
+        self._calc_result()
+    
+    def _calc_result(self):
+        ap = self.Aperture
+        tar = self.Target
+        if not all((ap, tar)):
+            return
+        
+        #maybe the object needs a source trait
+        #instead, just take the first source found
+        source = self._tracer.sources[0]
+        
+            
+        #a list of RayCollections
+        raysList = source.TracedRays
+        #print "list: ",raysList
+            
+        denom_count = get_total_intersections(raysList, tar)
+        nom_count = get_total_entries(raysList, ap)
+        
+
+        try:
+            self.result = float(nom_count)/float(denom_count)
+        except ZeroDivisionError:
+            self.result = numpy.Infinity
