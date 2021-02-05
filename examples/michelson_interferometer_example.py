67f6b0d7c0d11766d4d49e2d268ed43d48d37cf7
bryancole <none@none>
2021-02-05 10:08:47 +0000

    Added michelson interferometer example (incomplete).

---
+++ examples/michelson_interferometer_example.py
@@ -0,0 +1,32 @@
+
+
+from raypier.api import RayTraceModel, UnpolarisingBeamsplitterCube, CollimatedGaussletSource, CircleShape, GeneralLens,\
+        SphericalFace, PlanarFace, OpticalMaterial, PolarisingBeamsplitterCube, ParallelRaySource
+        
+        
+
+src=CollimatedGaussletSource(origin=(-30,0,0),
+                             direction=(1,0,0),
+                             radius=1.0,
+                             resolution=1.5,
+                             E_vector=(0,1,0),
+                             wavelength=1.0)
+
+# src = ParallelRaySource(origin=(-30,0,0),
+#                              direction=(1,0,0),
+#                              radius=1.0,
+#                              resolution=1.5,
+#                              E_vector=(0,1,0),
+#                              wavelength=1.0)
+        
+        
+bs = UnpolarisingBeamsplitterCube(centre=(0,0,0),
+                                  size=10.0)
+# bs = PolarisingBeamsplitterCube(centre=(0,0,0),
+#                                   size=10.0)
+
+
+model = RayTraceModel(optics=[bs],
+                      sources=[src])
+
+model.configure_traits()
